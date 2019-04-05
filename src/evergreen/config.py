91afacbb4d7041b6caa8fd17589b5dd6f4336263
David Bradford <david.bradford@mongodb.com>
2019-04-05 10:12:15 -0400

    Don't use cwd for evergreen config

--- src/evergreen/config.py
+++ src/evergreen/config.py
@@ -11,7 +11,6 @@ EvgAuth = namedtuple('EvgAuth', ['username', 'api_key'])
 
 DEFAULT_API_SERVER = 'http://evergreen.mongodb.com'
 CONFIG_FILE_LOCATIONS = [
-    os.path.join('.', '.evergreen.yml'),
     os.path.expanduser(os.path.join('~', '.evergreen.yml')),
     os.path.expanduser(os.path.join('~', 'cli_bin', '.evergreen.yml')),
 ]
