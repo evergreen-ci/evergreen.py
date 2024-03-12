e5ac15939e9da437e34b6c5587efc5e10ac89a48
Ron Neuman <31408835+rongodb@users.noreply.github.com>
2024-03-12 16:14:17 -0400

    DEVPROD-4784: Add caching support to tasks_by_build (#165)

--- src/evergreen/api.py
+++ src/evergreen/api.py
@@ -1505,11 +1505,27 @@ class CachedEvergreenApi(EvergreenApi):
         """
         return super(CachedEvergreenApi, self).version_by_id(version_id)
 
+    @lru_cache(maxsize=CACHE_SIZE)
+    def tasks_by_build(
+        self, build_id: str, fetch_all_executions: Optional[bool] = None
+    ) -> List[Task]:
+        """
+        Get tasks by build.
+
+        :param build_id: Id of build to query.
+        :param fetch_all_executions: should fetch all executions of the tasks
+        :return: List of the queried tasks.
+        """
+        return super(CachedEvergreenApi, self).tasks_by_build(
+            build_id=build_id, fetch_all_executions=fetch_all_executions
+        )
+
     def clear_caches(self) -> None:
         """Clear the cache."""
         cached_functions = [
             self.build_by_id,
             self.version_by_id,
+            self.tasks_by_build,
         ]
         for fn in cached_functions:
             fn.cache_clear()  # type: ignore[attr-defined]
