7c59693506929c2c5b17ac383878d73160198dcc
John Daniels <john.daniels@mongodb.com>
2024-05-29 17:12:53 +0000

    DEVPROD-6266: Add  changelog information. (#177)

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.6.28',
+    VERSION: '3.6.29',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
